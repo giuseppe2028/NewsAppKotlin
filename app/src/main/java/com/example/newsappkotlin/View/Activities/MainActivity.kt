5abb7530d6787be5f357721d3acec24f1060a2a5
Giuseppe Barone <78317934+giuseppe2028@users.noreply.github.com>
2023-06-20 14:10:11 +0200

    Sistemazione Schermate

---
+++ app/src/main/java/com/example/newsappkotlin/View/Activities/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.newsappkotlin.View.Activities
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.newsappkotlin.R
+import com.example.newsappkotlin.View.Fragments.Home_fragment
+import com.example.newsappkotlin.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        navSelector()
+    }
+
+    private fun navSelector(){
+        val manager = supportFragmentManager
+        val transaction = manager.beginTransaction()
+        val selectItem = binding.navigationBar.selectedItemId
+        when(selectItem){
+            R.id.news -> transaction.replace(binding.fragmentContainerHome.id,Home_fragment())
+
+        }
+    }
+}
