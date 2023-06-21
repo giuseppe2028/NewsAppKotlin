ac1ce40e3a87f027c9782c6058ac84ad2bd253b3
Giuseppe Barone <78317934+giuseppe2028@users.noreply.github.com>
2023-06-21 18:51:14 +0200

    Implementazione cardSchermata

---
+++ app/src/main/java/com/example/newsappkotlin/View/Adapter/HomeNewsAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.newsappkotlin.View.Adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.newsappkotlin.View.Model.News
+
+class HomeNewsAdapter(val lista:ArrayList<News>):RecyclerView.Adapter<> {
+
+    class ViewHolder(val binding): RecyclerView.ViewHolder() {
+
+    }
+}
