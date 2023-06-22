48b9c2644f40e482434c0dab12941733d1b66d12
Giuseppe Barone <78317934+giuseppe2028@users.noreply.github.com>
2023-06-22 19:42:30 +0200

    Sistemazione Card notizia

--- app/src/main/java/com/example/newsappkotlin/View/Adapter/HomeNewsAdapter.kt
+++ app/src/main/java/com/example/newsappkotlin/View/Adapter/HomeNewsAdapter.kt
@@ -3,7 +3,7 @@ package com.example.newsappkotlin.View.Adapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.newsappkotlin.View.Model.News
 
-class HomeNewsAdapter(val lista:ArrayList<News>):RecyclerView.Adapter<> {
+class HomeNewsAdapter(val lista:ArrayList<News>):RecyclerView.Adapter<HomeNewsAdapter.ViewHolder>() {
 
     class ViewHolder(val binding): RecyclerView.ViewHolder() {
 
